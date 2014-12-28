7ee6c7b5a1b9dbf55cb66367e0ed2736e7ed07ca
maclovr <maclovr75@gmaill.com>
2014-12-28 04:51:48 +0100

    Refactored extensions initialization

--- src/Framework/Application.php
+++ src/Framework/Application.php
@@ -4,9 +4,9 @@ namespace Nkstamina\Framework;
 use Nkstamina\Framework\Common\Utils;
 use Nkstamina\Framework\Controller\ControllerResolver;
 use Nkstamina\Framework\Provider\ConfigServiceProvider;
+use Nkstamina\Framework\Provider\DatabaseServiceProvider;
 use Nkstamina\Framework\Provider\RoutingServiceProvider;
 use Nkstamina\Framework\Provider\TemplatingServiceProvider;
-use Nkstamina\Framework\Provider\ExtensionServiceProvider;
 use Pimple\ServiceProviderInterface;
 use Pimple\Container;
 use Symfony\Component\HttpFoundation\Request;
@@ -22,14 +22,25 @@ use Symfony\Component\Yaml\Yaml;
  * Class Application
  * @package Nkstamina\Framework
  */
-class Application extends Container implements HttpKernelInterface
+abstract class Application extends Container implements HttpKernelInterface, ApplicationInterface
 {
-    const VERSION = '0.0.0-DEV';
-
+    const VERSION = '0.1.0-DEV';
     const EARLY_EVENT = 512;
     const LATE_EVENT  = -512;
 
+    /**
+     * @var array An array of providers
+     */
     protected $providers = [];
+
+    /**
+     * @var array An array of extensions
+     */
+    protected $extensions = [];
+
+    /**
+     * @var bool Is application already booted?
+     */
     protected $booted = false;
 
     /**
@@ -54,7 +65,7 @@ class Application extends Container implements HttpKernelInterface
         $this['twig.cache.directory'] = $this['app.cache.dir'].'/templates';
         $this['twig.cache_templates'] = false;
 
-        // to switch between prod & dev
+        // To switch between prod & dev
         // just set the APP_ENV environment variable:
         // in apache: SetEnv APP_ENV dev
         // in nginx with fcgi: fastcgi_param APP_ENV dev
@@ -83,40 +94,23 @@ class Application extends Container implements HttpKernelInterface
         $this['request_error'] = $this->protect(function () {
             throw new \RuntimeException('Accessed request service outside of request scope. Try moving that call to a before handler or controller.');
         });
+
         $this['request'] = $this['request_error'];
 
         $this->register(new ConfigServiceProvider($app));
         $this->register(new RoutingServiceProvider($app));
         $this->register(new TemplatingServiceProvider($app));
-        $this->register(new ExtensionServiceProvider($app));
+        $this->register(new DatabaseServiceProvider($app));
 
-        // loads App configuration parameters
+        // load Application's configuration parameters
         $this['app.parameters'] = $app->factory(function () use ($app) {
-            $parameters = [];
-
-            if (Utils::isDirectoryValid($app['app.config.dir'])) {
-                $files = $app['config.finder']
-                    ->files()
-                    ->name('*.yml')
-                    ->in($app['app.config.dir'])
-                    ->in($app['app.extensions.dir'])
-                ;
-
-                $yaml = $app['config.parser'];
-
-                foreach($files as $file) {
-                    try {
-                        $parameters[$file->getRelativePathname()] = [$yaml->parse(file_get_contents($file->getRealpath()))];
-
-                    } catch(ParseException $e) {
-                        printf("Unable to parse the YAML string: %s", $e->getMessage());
-                    }
-                }
-            }
-
-            return $parameters;
+            return $this->loadApplicationParameters($app);
         });
 
+        // Load Application's extensions
+        $this['app.extensions'] = $app->factory(function () use ($app) {
+            return $this->initializeExtensions($app);
+        });
 
         $this['dispatcher']->addSubscriber(new RouterListener($app['matcher']));
 
@@ -153,6 +147,10 @@ class Application extends Container implements HttpKernelInterface
         if (!$this->booted) {
             foreach ($this->providers as $provider) {
                 $provider->boot($this);
+            }
+
+            foreach($this->extensions as $extension) {
+                $extension->boot($this);
             }
         }
 
@@ -236,5 +234,69 @@ class Application extends Container implements HttpKernelInterface
     public function getValue($key)
     {
         return $this[$key];
+    }
+
+    /**
+     * Initialize extensions
+     *
+     * @throws \LogicException
+     */
+    protected function initializeExtensions()
+    {
+        $this->extensions = [];
+
+        foreach($this->registerExtensions() as $extension) {
+            $name = $extension->getName();
+
+            if (isset($this->extensions[$name])) {
+                throw new \LogicException(sprintf('Trying to register two extensions with the same name "%s"', $name));
+            }
+
+            if (0 === strpos($extension->getPath(), 'extensions')) {
+                throw new \LogicException(sprintf('The extension "%s" shoud be installed in the "%s" directory', $name, $this['app.extensions.dir']));
+            }
+
+            $this->extensions[$name] = $extension;
+        }
+
+        return $this->extensions;
+    }
+
+
+    /**
+     * Loads application's parameters
+     *
+     * @param Application $app
+     *
+     * @return array
+     */
+    protected function loadApplicationParameters(Application $app)
+    {
+        $parameters = [];
+
+        if (Utils::isDirectoryValid($app['app.config.dir'])) {
+            $files = $app['config.finder']
+                ->files()
+                ->name('*.yml')
+                ->in($app['app.config.dir'])
+            ;
+
+            $yaml = $app['config.parser'];
+
+            foreach ($files as $file) {
+                try {
+                    $parameters[$file->getRelativePathname()] = [$yaml->parse(file_get_contents($file->getRealpath()))];
+                } catch (ParseException $e) {
+                    printf("Unable to parse the YAML string: %s", $e->getMessage());
+                }
+            }
+        }
+
+        return $parameters;
+    }
+
+    protected function getExtensions(Application $app)
+    {
+        return $app['app.extensions'];
     }
 }
